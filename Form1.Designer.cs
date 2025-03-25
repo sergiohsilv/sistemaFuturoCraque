cea0ae13ab9a3472a6f1f431ab3b4bc42962ff37
SERGIO HENRIQUE DA SILVA <sergiosilva01505@gmail.com>
2025-03-25 11:42:33 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace sistemaFuturoCraque
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.Lavender;
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtUsuario.Location = new System.Drawing.Point(34, 225);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(250, 26);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BackColor = System.Drawing.Color.Lavender;
+            this.txtSenha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(34, 299);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(250, 26);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.MediumBlue;
+            this.btnLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.Snow;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnLogin.Location = new System.Drawing.Point(110, 371);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(111, 31);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(35, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(35, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Senha";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(128, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Login";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(466, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(259, 42);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Futuro Craque";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(513, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Escola de Futebol";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(53, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(228, 42);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "BEM VINDO";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::sistemaFuturoCraque.Properties.Resources.Futuro_Craque__1__removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(353, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ frmTelaInicial.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace sistemaFuturoCraque
+{
+    partial class frmTelaInicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnCadastroAluno = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnCadastroFuncionario = new System.Windows.Forms.Button();
+            this.btnCadastroEquipe = new System.Windows.Forms.Button();
+            this.btnCadastroJogos = new System.Windows.Forms.Button();
+            this.btnCadastroEquipamentos = new System.Windows.Forms.Button();
+            this.btnCadastroReserva = new System.Windows.Forms.Button();
+            this.btnRelatorio = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(10, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Futuro Craque";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(16, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Escola de Futebol";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::sistemaFuturoCraque.Properties.Resources.Captura_de_tela_2025_03_13_083650;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 444);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::sistemaFuturoCraque.Properties.Resources.Futuro_Craque__1_;
+            this.pictureBox2.Location = new System.Drawing.Point(1, -9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(143, 125);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnCadastroAluno
+            // 
+            this.btnCadastroAluno.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCadastroAluno.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCadastroAluno.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnCadastroAluno.Location = new System.Drawing.Point(12, 164);
+            this.btnCadastroAluno.Name = "btnCadastroAluno";
+            this.btnCadastroAluno.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnCadastroAluno.Size = new System.Drawing.Size(120, 23);
+            this.btnCadastroAluno.TabIndex = 4;
+            this.btnCadastroAluno.Text = "Alunos";
+            this.btnCadastroAluno.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::sistemaFuturoCraque.Properties.Resources.Futuro_Craque_removebg_preview;
+            this.pictureBox3.Location = new System.Drawing.Point(272, 58);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnCadastroFuncionario
+            // 
+            this.btnCadastroFuncionario.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCadastroFuncionario.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCadastroFuncionario.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnCadastroFuncionario.Location = new System.Drawing.Point(12, 198);
+            this.btnCadastroFuncionario.Name = "btnCadastroFuncionario";
+            this.btnCadastroFuncionario.Size = new System.Drawing.Size(120, 23);
+            this.btnCadastroFuncionario.TabIndex = 6;
+            this.btnCadastroFuncionario.Text = "Funcionários";
+            this.btnCadastroFuncionario.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastroEquipe
+            // 
+            this.btnCadastroEquipe.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCadastroEquipe.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCadastroEquipe.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnCadastroEquipe.Location = new System.Drawing.Point(12, 232);
+            this.btnCadastroEquipe.Name = "btnCadastroEquipe";
+            this.btnCadastroEquipe.Size = new System.Drawing.Size(120, 23);
+            this.btnCadastroEquipe.TabIndex = 7;
+            this.btnCadastroEquipe.Text = "Equipes";
+            this.btnCadastroEquipe.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastroJogos
+            // 
+            this.btnCadastroJogos.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCadastroJogos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCadastroJogos.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnCadastroJogos.Location = new System.Drawing.Point(12, 264);
+            this.btnCadastroJogos.Name = "btnCadastroJogos";
+            this.btnCadastroJogos.Size = new System.Drawing.Size(120, 23);
+            this.btnCadastroJogos.TabIndex = 8;
+            this.btnCadastroJogos.Text = "Jogos";
+            this.btnCadastroJogos.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastroEquipamentos
+            // 
+            this.btnCadastroEquipamentos.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCadastroEquipamentos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCadastroEquipamentos.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnCadastroEquipamentos.Location = new System.Drawing.Point(12, 296);
+            this.btnCadastroEquipamentos.Name = "btnCadastroEquipamentos";
+            this.btnCadastroEquipamentos.Size = new System.Drawing.Size(120, 23);
+            this.btnCadastroEquipamentos.TabIndex = 9;
+            this.btnCadastroEquipamentos.Text = "Equipamentos";
+            this.btnCadastroEquipamentos.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastroReserva
+            // 
+            this.btnCadastroReserva.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCadastroReserva.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCadastroReserva.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnCadastroReserva.Location = new System.Drawing.Point(14, 330);
+            this.btnCadastroReserva.Name = "btnCadastroReserva";
+            this.btnCadastroReserva.Size = new System.Drawing.Size(118, 23);
+            this.btnCadastroReserva.TabIndex = 10;
+            this.btnCadastroReserva.Text = "Reserva";
+            this.btnCadastroReserva.UseVisualStyleBackColor = false;
+            // 
+            // btnRelatorio
+            // 
+            this.btnRelatorio.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnRelatorio.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRelatorio.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnRelatorio.Location = new System.Drawing.Point(14, 367);
+            this.btnRelatorio.Name = "btnRelatorio";
+            this.btnRelatorio.Size = new System.Drawing.Size(118, 23);
+            this.btnRelatorio.TabIndex = 11;
+            this.btnRelatorio.Text = "Relatório";
+            this.btnRelatorio.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.DimGray;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSair.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnSair.Location = new System.Drawing.Point(14, 495);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(118, 23);
+            this.btnSair.TabIndex = 12;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // frmTelaInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 530);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnRelatorio);
+            this.Controls.Add(this.btnCadastroReserva);
+            this.Controls.Add(this.btnCadastroEquipamentos);
+            this.Controls.Add(this.btnCadastroJogos);
+            this.Controls.Add(this.btnCadastroEquipe);
+            this.Controls.Add(this.btnCadastroFuncionario);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnCadastroAluno);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmTelaInicial";
+            this.Text = "frmTelaInicial";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnCadastroAluno;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnCadastroFuncionario;
+        private System.Windows.Forms.Button btnCadastroEquipe;
+        private System.Windows.Forms.Button btnCadastroJogos;
+        private System.Windows.Forms.Button btnCadastroEquipamentos;
+        private System.Windows.Forms.Button btnCadastroReserva;
+        private System.Windows.Forms.Button btnRelatorio;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
